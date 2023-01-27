52641077d3b8af3e74b0dc6855c8ef94803abc8f
陈柒 <3062995371@qq.com>
2023-01-27 23:50:40 +0800

    faile

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,21 +1,24 @@
 import Theme from "vitepress/theme";
 import { coloricon } from "../compoment/navbar/coloricon";
 import "./style/var.css";
-
+import * as buffer from "buffer";
+if (typeof (window as any).global === "undefined") {
+  (window as any).global = window;
+}
+if (typeof (window as any).Buffer === "undefined") {
+  (window as any).Buffer = buffer.Buffer;
+}
 export default {
   ...Theme,
   enhanceApp({ app }) {
     //脚本
     let url = "";
     setInterval(() => {
-      if (
-        window &&
-        window.location &&
-        window.location.href &&
-        url != window.location.href
-      ) {
-        coloricon();
-        url = window.location.href;
+      if (typeof (window as any).global !== "undefined") {
+        if (url != window.location.href) {
+          coloricon();
+          url = window.location.href;
+        }
       }
     }, 500);
   },
