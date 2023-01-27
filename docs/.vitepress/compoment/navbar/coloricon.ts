6b01710ebfbf9711d8541bb5945c5c12318ebb5f
陈柒 <3062995371@qq.com>
2023-01-28 00:43:06 +0800

    success

--- docs/.vitepress/compoment/navbar/coloricon.ts
+++ docs/.vitepress/compoment/navbar/coloricon.ts
@@ -1,42 +1,60 @@
 export const coloricon = () => {
-  setTimeout(() => {
-    const items = document.getElementsByClassName("items");
-    if (items) {
-      for (let i = 0; i < items.length; i++) {
-        const as = items[i].getElementsByClassName("VPLink");
-        for (let j = 0; j < as.length; j++) {
-          if (as[j].innerHTML.indexOf("v-u-e") !== -1) {
-            as[
-              j
-            ].innerHTML = `<div style="display:flex;"><img style="margin-right:8px;" src="/icon/vue.svg" alt="null" p-id="1416">Vue<div>`;
+  let url = "";
+  setInterval(() => {
+    // (function (window: any) {
+    if (typeof (window as any) !== "undefined") {
+      if (
+        window &&
+        window.location &&
+        window.location.href &&
+        url != window.location.href
+      ) {
+        setTimeout(() => {
+          const items = document.getElementsByClassName("items");
+          if (items) {
+            for (let i = 0; i < items.length; i++) {
+              const as = items[i].getElementsByClassName("VPLink");
+              for (let j = 0; j < as.length; j++) {
+                if (as[j].innerHTML.indexOf("v-u-e") !== -1) {
+                  as[
+                    j
+                  ].innerHTML = `<div style="display:flex;"><img style="margin-right:8px;" src="/icon/vue.svg" alt="null" p-id="1416">Vue<div>`;
+                }
+                if (as[j].innerHTML.indexOf("t-s") !== -1) {
+                  as[
+                    j
+                  ].innerHTML = `<div style="display:flex;"><img style="margin-right:8px;" src="/icon/typescript.svg" alt="null" p-id="1416">TS<div>`;
+                }
+                if (as[j].innerHTML.indexOf("d-a-r-t") !== -1) {
+                  as[
+                    j
+                  ].innerHTML = `<div style="display:flex;"><img style="margin-right:8px;" src="/icon/dart.svg" alt="null" p-id="1416">Dart<div>`;
+                }
+                if (as[j].innerHTML.indexOf("f-l-u-t-t-e-r") !== -1) {
+                  as[
+                    j
+                  ].innerHTML = `<div style="display:flex;"><img style="margin-right:8px;" src="/icon/flutter.svg" alt="null" p-id="1416">Flutter<div>`;
+                }
+                if (as[j].innerHTML.indexOf("h-t-m-l") !== -1) {
+                  as[
+                    j
+                  ].innerHTML = `<div style="display:flex;"><img style="margin-right:8px;" src="/icon/HTML.svg" alt="null" p-id="1416">HTML<div>`;
+                }
+                if (as[j].innerHTML.indexOf("c-s-s") !== -1) {
+                  as[
+                    j
+                  ].innerHTML = `<div style="display:flex;"><img style="margin-right:8px;" src="/icon/css.svg" alt="null" p-id="1416">CSS<div>`;
+                }
+              }
+            }
           }
-          if (as[j].innerHTML.indexOf("t-s") !== -1) {
-            as[
-              j
-            ].innerHTML = `<div style="display:flex;"><img style="margin-right:8px;" src="/icon/typescript.svg" alt="null" p-id="1416">TS<div>`;
-          }
-          if (as[j].innerHTML.indexOf("d-a-r-t") !== -1) {
-            as[
-              j
-            ].innerHTML = `<div style="display:flex;"><img style="margin-right:8px;" src="/icon/dart.svg" alt="null" p-id="1416">Dart<div>`;
-          }
-          if (as[j].innerHTML.indexOf("f-l-u-t-t-e-r") !== -1) {
-            as[
-              j
-            ].innerHTML = `<div style="display:flex;"><img style="margin-right:8px;" src="/icon/flutter.svg" alt="null" p-id="1416">Flutter<div>`;
-          }
-          if (as[j].innerHTML.indexOf("h-t-m-l") !== -1) {
-            as[
-              j
-            ].innerHTML = `<div style="display:flex;"><img style="margin-right:8px;" src="/icon/HTML.svg" alt="null" p-id="1416">HTML<div>`;
-          }
-          if (as[j].innerHTML.indexOf("c-s-s") !== -1) {
-            as[
-              j
-            ].innerHTML = `<div style="display:flex;"><img style="margin-right:8px;" src="/icon/css.svg" alt="null" p-id="1416">CSS<div>`;
-          }
-        }
+        }, 0);
+        url = window.location.href;
       }
     }
-  }, 0);
+    /* Keep source code the same */
+    // })(typeof window == "undefined" ? global : window);
+    // or
+    // })(this);
+  }, 500);
 };
--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,35 +1,15 @@
-import Theme from "vitepress/theme";
-import { coloricon } from "../compoment/navbar/coloricon";
+import DefaultTheme from "vitepress/theme";
 import "./style/var.css";
-// import * as buffer from "buffer";
-// if (typeof (window as any).global === "undefined") {
-//   (window as any).global = window;
-// }
-// if (typeof (window as any).Buffer === "undefined") {
-//   (window as any).Buffer = buffer.Buffer;
-// }
+import { onMounted } from "vue";
+import { coloricon } from "../compoment/navbar/coloricon";
 export default {
-  ...Theme,
-  enhanceApp({ app }) {
-    //脚本
-    let url = "";
-    setInterval(() => {
-      (function (window: any) {
-        if (typeof (window as any) !== "undefined") {
-          if (
-            window &&
-            window.location &&
-            window.location.href &&
-            url != window.location.href
-          ) {
-            coloricon();
-            url = window.location.href;
-          }
-        }
-        /* Keep source code the same */
-        // })(typeof window == "undefined" ? global : window);
-        // or
-      })(this);
-    }, 500);
+  ...DefaultTheme,
+  // Nav: () => {},
+  enhanceApp({ ctx }) {
+  },
+  setup() {
+    onMounted(() => {
+      coloricon();
+    });
   },
 };
