9fa98ec14565a17068f630092877d10ed408a5c9
陈柒 <3062995371@qq.com>
2023-01-27 23:54:31 +0800

    007

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,25 +1,30 @@
 import Theme from "vitepress/theme";
 import { coloricon } from "../compoment/navbar/coloricon";
 import "./style/var.css";
-import * as buffer from "buffer";
-if (typeof (window as any).global === "undefined") {
-  (window as any).global = window;
-}
-if (typeof (window as any).Buffer === "undefined") {
-  (window as any).Buffer = buffer.Buffer;
-}
+// import * as buffer from "buffer";
+// if (typeof (window as any).global === "undefined") {
+//   (window as any).global = window;
+// }
+// if (typeof (window as any).Buffer === "undefined") {
+//   (window as any).Buffer = buffer.Buffer;
+// }
 export default {
   ...Theme,
   enhanceApp({ app }) {
     //脚本
     let url = "";
     setInterval(() => {
-      if (typeof (window as any).global !== "undefined") {
-        if (url != window.location.href) {
-          coloricon();
-          url = window.location.href;
+      (function (window: any) {
+        if (typeof (window as any) !== "undefined") {
+          if (url != window.location.href) {
+            coloricon();
+            url = window.location.href;
+          }
         }
-      }
+        /* Keep source code the same */
+        // })(typeof window == "undefined" ? global : window);
+        // or
+      })(this);
     }, 500);
   },
 };
